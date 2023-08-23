6ef3fd6c4c326c830968ef081f96917ebe8919a4
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-08-23 09:20:29 +0700

    update

--- src/models/database-connect.ts
+++ src/models/database-connect.ts
@@ -1,8 +1,9 @@
 import * as mongoose from "mongoose";
+import * as process from "process";
 
 class DatabaseConnect {
     static async connectDB() {
-        const DB_URL = 'mongodb+srv://langochai11121998:langochai1998@cluster0.odkiv4a.mongodb.net/music';
+        const DB_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.odkiv4a.mongodb.net/music`;
         return await mongoose.connect(DB_URL);
     }
 }
